3ed5410d252188c8bf1a6da3f8646957eabfdd26
waheiiiddd-lab <waheiiiddd@gmail.com>
2026-03-27 03:44:48 +0700

    Refactor ExtremeTileService for command execution

--- app/src/main/java/com/zixine/engine/ExtremeTileService.java
+++ app/src/main/java/com/zixine/engine/ExtremeTileService.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 import java.io.DataOutputStream;
 
 public class ExtremeTileService extends TileService {
-    private String EXCLUDES = "com.zcqptx.dcwihze|com.termux|android|com.android.systemui|com.miui.home|com.zixine.engine|com.android.settings|com.dts.freefireth|com.dts.freefiremax|com.mobile.legends|com.tencent.ig|com.pubg.imobile|com.miHoYo.GenshinImpact|com.hoYoverse.hkrpg|com.riotgames.league.wildrift|com.garena.game.codm";
+    private String EXCLUDES = "com.zcqptx.dcwihze|com.termux|android|com.android.systemui|com.miui.home|com.zixine.engine|com.android.settings|com.dts.freefireth|com.dts.freefiremax|com.mobile.legends|com.tencent.ig|com.pubg.imobile|com.miHoYo.GenshinImpact|com.hoYoverse.hkrpg";
 
     @Override
     public void onClick() {
@@ -14,23 +14,27 @@ public class ExtremeTileService extends TileService {
         boolean active = (t.getState() == Tile.STATE_INACTIVE);
 
         if (active) {
-            String cmd = "PKGS=$(pm list packages -e | cut -d ':' -f2 | grep -Ev '" + EXCLUDES + "'); for p in $PKGS; do pm suspend $p & done; pm disable com.miui.powerkeeper/.statemachine.PowerStateMachineService &";
+            // EXTREME ON: Filter app, Suspend satu-satu secara berurutan agar tidak ada yang terlewat
+            String cmd = "PKGS=$(pm list packages -e | cut -d ':' -f2 | grep -Ev '" + EXCLUDES + "'); " +
+                         "for p in $PKGS; do pm suspend --user 0 $p; done; " +
+                         "am kill-all; " +
+                         "pm disable com.miui.powerkeeper/.statemachine.PowerStateMachineService;";
             exec(cmd);
-            t.setState(Tile.STATE_ACTIVE); // Warna Nyala
-            Toast.makeText(this, "EXTREME MODE: ACTIVE ⚡", Toast.LENGTH_SHORT).show();
+            t.setState(Tile.STATE_ACTIVE);
+            Toast.makeText(this, "SYSTEM: SEALED 🛡️", 0).show();
         } else {
-            exec("PKGS=$(pm list packages -u | cut -d ':' -f2); for p in $PKGS; do pm unsuspend $p & done; pm enable com.miui.powerkeeper/.statemachine.PowerStateMachineService &");
-            t.setState(Tile.STATE_INACTIVE); // Warna Mati
-            Toast.makeText(this, "EXTREME MODE: OFF 🌍", Toast.LENGTH_SHORT).show();
+            // EXTREME OFF: Bangunkan SEMUA
+            exec("PKGS=$(pm list packages -u | cut -d ':' -f2); for p in $PKGS; do pm unsuspend --user 0 $p; done; pm enable com.miui.powerkeeper/.statemachine.PowerStateMachineService;");
+            t.setState(Tile.STATE_INACTIVE);
+            Toast.makeText(this, "SYSTEM: NORMAL 🌍", 0).show();
         }
         t.updateTile();
     }
-
     private void exec(String c) {
         try {
             java.lang.Process p = Runtime.getRuntime().exec("su");
             DataOutputStream o = new DataOutputStream(p.getOutputStream());
             o.writeBytes(c + "\nexit\n"); o.flush();
-        } catch (Exception ignored) {}
+        } catch (Exception e) {}
     }
 }
