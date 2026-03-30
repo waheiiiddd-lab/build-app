0f73a041dc1a6698a1287201f28e060062c19821
waheiiiddd-lab <waheiiiddd@gmail.com>
2026-03-30 17:30:37 +0700

    Adjust refresh rate and animation scale settings

--- app/src/main/java/com/zixine/engine/PerfTileService.java
+++ app/src/main/java/com/zixine/engine/PerfTileService.java
@@ -20,7 +20,7 @@ public class PerfTileService extends TileService {
             "settings put system min_refresh_rate 120.0; settings put system peak_refresh_rate 120.0; " +
             "settings put global window_animation_scale 0; setprop touch.pressure.scale 0.001; " +
             "setprop debug.touch.filter 0; resetprop ro.min.fling_velocity 8000; killall -STOP thermald;" : 
-            "settings put system min_refresh_rate 60.0; settings put global window_animation_scale 1; " +
+            "settings put system min_refresh_rate 120.0; settings put global window_animation_scale 1; " +
             "setprop touch.pressure.scale 1; setprop debug.touch.filter 1; resetprop ro.min.fling_velocity 50; killall -CONT thermald;";
         
         new Thread(() -> {
